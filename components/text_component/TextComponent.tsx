d89164d649f891b268ba470064f86b3ce1d0123e
Pablo <pablosalgadou@gmail.com>
2022-12-01 15:42:04 -0300

    add testing initial layout

--- components/text_component/TextComponent.tsx
+++ components/text_component/TextComponent.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 
 interface TextProps {
-  text: string ;
-  background: string ;
+  text: string | null;
+  background: string | null;
 }
 
-const TextComponents = ({
-  text = 'Agegue texto',
+const TextComponent = ({
+  text = 'Agregue texto',
   background = 'bg-slate-200',
 }: TextProps) => {
   return (
-    <div className={` ${background} w-[300px] border-white h-[50px] rounded-xl text-center flex items-center justify-items-center`}>
-      <h1 className="">{text}</h1>
+    <div
+      className={` ${background} w-[300px] border-white h-[50px] rounded-xl text-center flex items-center justify-items-center`}
+    >
+      <h1 data-testid="textComponent" className="">
+        {text}
+      </h1>
     </div>
   );
 };
 
-export default TextComponents;
+export default TextComponent;
